41fbf6e330e94a83505dd415667231b0551e25d8
unknown <ahmedadel18182@gmail.com>
2022-08-20 08:36:21 +0300

    Done

---
+++ index.js
@@ -0,0 +1,29 @@
+let progress = document.querySelector(".pro");
+let circleValue = document.querySelector(".in-circle");
+let inp = document.querySelector(".input");
+let btn = document.querySelector(".btn");
+
+let i = 1;
+
+btn.addEventListener("click", () => {
+  if (inp.value !== "") {
+    let holder = setInterval(() => {
+      if (i < inp.value) {
+        circleValue.textContent = `${i++ + 1}%`;
+        progress.style.background = `conic-gradient(rgb(161, 16, 161) ${i}deg, #979191 0deg)`;
+      }
+      if (i > inp.value) {
+        circleValue.textContent = `${i-- - 1}%`;
+        progress.style.background = `conic-gradient(rgb(161, 16, 161) ${i}deg, #979191 0deg)`;
+      }
+      if (i == inp.value) {
+        clearInterval(holder);
+      }
+      if (circleValue.textContent == "360%") {
+        circleValue.innerHTML = `<img src="images/true.jpg" alt="" class="true">`;
+      }
+    }, 10);
+  } else {
+    window.alert("You Must Fill This Input With Numbers From 0 To 360");
+  }
+});
